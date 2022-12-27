749c781da6041cd2e2fa6a4da0fab5a6de90916c
Mohamed Anis MANI <manimohamed@gmail.com>
2022-12-27 17:24:20 +0100

    read xml files

--- downloads/notes_db/readxml.py
+++ downloads/notes_db/readxml.py
@@ -1,3 +1,4 @@
+from pprint import pprint
 import xml.dom.minidom as dom
 import os
 
@@ -44,19 +45,62 @@ def general_data(document):
         infos[tagname] = getText(document.getElementsByTagName(tagname)[0])
     return infos
 
+
+def read_xml_file(xmlfile):
+    doc = dom.parse(xmlfile)
+    infos = general_data(doc)
+    eleves = liste_notes(doc)
+    epreuves = liste_epreuves(doc)
+    return {
+        'generalInfos': infos,
+        'eleves': eleves,
+        'epreuves': epreuves
+    }
+
+
+class NotesClasse:
+    ELEVES_KEYS = ['IDENELEV', 'numOrdre', 'prenomnom']
+    CLASSE_KEYS = ['codeclass', 'codedre', 'codeeetab', 'drear',
+                   'libeclass', 'libeetab', 'codeperiodexam', 'libperiodexam']
+    EPREUVES_KEYS = ['CODEMATI', 'CODETYPEEPRE', 'CODETYPEMATI',
+                     'NUMEEPRE', 'libTypeEpr', 'abretypeeprear']
+    NOTES_KEYS = ['CODEMATI', 'CODETYPEEPRE', 'CODETYPEMATI',
+                  'IDENELEV', 'NUMEEPRE', 'NOTEEPRE']
+
+    def __init__(self, xmlData):
+        self.xmlData = xmlData
+        self.matiere = xmlData['epreuves'][0]['libeMat']
+        self.eleves = [{key: eleve[key] for key in NotesClasse.ELEVES_KEYS}
+                       for eleve in xmlData['eleves']]
+        self.eleves = [eleve
+                       for idx, eleve in enumerate(self.eleves)
+                       if self.eleves.index(eleve) == idx]
+        self.classe = {key: value
+                       for key, value in xmlData['generalInfos'].items()
+                       if key in NotesClasse.CLASSE_KEYS}
+        self.epreuves = [{key: epreuve[key] for key in NotesClasse.EPREUVES_KEYS}
+                         for epreuve in xmlData['epreuves']]
+        self.notes = [{key: eleve[key] for key in NotesClasse.NOTES_KEYS}
+                      for eleve in xmlData['eleves']]
+        for eleve in self.eleves:
+            eleve['notes'] = [dict() for _ in self.epreuves]
+            notes = [note for note in self.notes
+                     if note['IDENELEV'] == eleve['IDENELEV']]
+            for idx, epreuve in enumerate(self.epreuves):
+                eleve['notes'][idx] = {epreuve['libTypeEpr']: [note for note in notes
+                                       if note['CODEMATI'] == epreuve['CODEMATI'] and
+                                       note['CODETYPEEPRE'] == epreuve['CODETYPEEPRE'] and
+                                       note['CODETYPEMATI'] == epreuve['CODETYPEMATI'] and
+                                       note['NUMEEPRE'] == note['NUMEEPRE']][0]['NOTEEPRE']}
+
+        pprint(self.eleves[0])
+
+
+filename = "0077143288_2003_31231301_31.xml"
+#filename = "0077143288_2001_31230202_31.xml"
 cur_dir = os.path.dirname(__file__)
-cur_file = os.path.join(cur_dir, "0077143288_2003_31231301_31.xml")
-
-doc = dom.parse(cur_file)
-noteelevs = doc.getElementsByTagName("noteelev")
-
-print(dir(doc))
-print(dir(noteelevs))
-
-print("---")
-infos = general_data(doc)
-eleves = liste_notes(doc)
-epreuves = liste_epreuves(doc)
-print(infos)
-print(eleves)
-print(epreuves)
+xmlFilePath = os.path.join(cur_dir, filename)
+xmlData = read_xml_file(xmlFilePath)
+# with open(xmlFilePath + '.py', "w", encoding="utf-8") as f:
+#     pprint.pprint(xmlData, f)
+notes = NotesClasse(xmlData)
